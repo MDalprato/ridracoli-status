cb7180301c7e622a031b479b7e600b9eb0e0dc5a
mdalprato-arteco <mdalprato@arteco-global.com>
2022-08-31 17:29:37 +0200

    Find error but ok

--- src/App.js
+++ src/App.js
@@ -28,25 +28,39 @@ const App = () => {
   const percVolumeLevel = Math.round(currentVolumeLevel * 100 / maxVolumeLevel).toFixed(2);
   const percVolumeLevelForGauge = Math.round(currentVolumeLevel * 100 / maxVolumeLevel).toFixed(2) / 100;
 
-
+  const tempAirForGauge = ridracoliObj.idrometeoRidraccoli.CtemperaturaAria / 100;
 
   return (
     <div className="App">
-      <div className="info">
-        <h1>Diga di ridracoli</h1>
-        <p>Livello invaso: {ridracoliObj.livelloInvaso}</p>
-        {/* <p>deflussiVolumeTotale: {ridracoliObj.ridraccoli.deflussiVolumeTotale}</p>
-      <p>condizioniAtmosferiche: {ridracoliObj.ridraccoli.condizioniAtmosferiche}</p>
-       */}
-        <p>currentVolumeLevel = {currentVolumeLevel}</p>
-        <p>Percentuale invaso: {percVolumeLevel} % </p>
+      <div className="volume-info">
+        <div className="info">
+          <p>Livello invaso: {ridracoliObj.livelloInvaso} m</p>
+          <p>Livello volume = {currentVolumeLevel} s.l.m</p>
+        </div>
+        <div className="percentage-char">
+          <p>Percentuale invaso</p>
+          <GaugeChart id="gauge-chart1"
+            nrOfLevels={10}
+            percent={percVolumeLevelForGauge}
+          />
+        </div>
       </div>
 
-    
-      <GaugeChart id="gauge-chart2"
-        nrOfLevels={10}
-        percent={percVolumeLevelForGauge}
-      />
+
+      <div className="temperature-info">
+        <div className="info">
+          <p>Temperatura aria: {ridracoliObj.idrometeoRidraccoli.CtemperaturaAria}</p>
+          <p>Umidità = {ridracoliObj.idrometeoRidraccoli.Dumidita}</p>
+          <p>Pressione atmosferica: {ridracoliObj.idrometeoRidraccoli.DpressioneAtmosferica} mb</p>
+        </div>
+        <div className="percentage-char">
+          <p>Percentuale invaso</p>
+          <GaugeChart id="gauge-chart2"
+            nrOfLevels={10}
+            percent={tempAirForGauge}
+          />
+        </div>
+      </div>
 
     </div>
   );
